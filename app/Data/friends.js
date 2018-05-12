557752257dfaae41e29416cd137dca5e9b002ae3
Camilo Gonzalez <CA@Cashs-MacBook-Pro.local>
2018-05-11 23:58:23 -0400

    site not functional yet. First commit with basic layout.

---
+++ app/Data/friends.js
@@ -0,0 +1,165 @@
+// Data structure that will store the friends data
+var friendsArray = [
+	{
+		name: 'Britney',
+		photo: 'http://www1.pictures.zimbio.com/bg/29th+Annual+GLAAD+Media+Awards+Los+Angeles+Vfcaw9kuht7l.jpg',
+		scores: [
+			'1',
+			'5',
+			'5',
+			'1',
+			'5',
+			'5',
+			'1',
+			'1',
+			'1',
+			'5'
+		]
+	},
+	{
+		name: 'Juan',
+		photo: 'https://vignette.wikia.nocookie.net/paw-patrol/images/1/12/David-lopez-3.jpg/revision/latest/scale-to-width-down/576?cb=20180210160744',
+		scores: [
+			'2',
+			'4',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'2',
+			'1',
+			'5'
+		]
+	},
+	{
+		name: 'Leonardo',
+		photo: 'https://ia.media-imdb.com/images/M/MV5BMjI0MTg3MzI0M15BMl5BanBnXkFtZTcwMzQyODU2Mw@@._V1_UY317_CR10,0,214,317_AL_.jpg',
+		scores: [
+			'4',
+			'3',
+			'5',
+			'1',
+			'1',
+			'5',
+			'5',
+			'1',
+			'1',
+			'1'
+		]
+	},
+	{
+		name: 'John',
+		photo: 'https://upload.wikimedia.org/wikipedia/commons/thumb/b/b7/John_Travolta_Deauville_2013_2.jpg/220px-John_Travolta_Deauville_2013_2.jpg',
+		scores: [
+			'5',
+			'1',
+			'2',
+			'1',
+			'3',
+			'4',
+			'1',
+			'3',
+			'3',
+			'4'
+		]
+	},
+	{
+		name: 'Peter',
+		photo: 'https://vignette.wikia.nocookie.net/gameofthrones/images/f/fd/Peter_2017.jpg/revision/latest/scale-to-width-down/331?cb=20170818054718',
+		scores: [
+			'1',
+			'1',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'1'
+		]
+	},
+	{
+		name: 'Carlos',
+		photo: 'https://envisiontec.com/wp-content/uploads/2016/11/carlos-210x300.jpg',
+		scores: [
+			'1',
+			'1',
+			'5',
+			'1',
+			'3',
+			'2',
+			'5',
+			'1',
+			'1',
+			'5'
+		]
+	},
+	{
+		name: 'Christina',
+		photo: 'https://caperiodista.com/wp-content/uploads/2018/03/tras-20-anos-en-los-escenarios-usando-maquillaje-christina-aguilera-realiza-una-sesion-sin-el-y-no-se-la-reconoce.jpg',
+		scores: [
+			'5',
+			'4',
+			'4',
+			'2',
+			'4',
+			'4',
+			'3',
+			'2',
+			'3',
+			'3'
+		]
+	},
+	{
+		name: 'Antonio',
+		photo: 'https://www.biography.com/.image/ar_1:1%2Cc_fill%2Ccs_srgb%2Cg_face%2Cq_auto:good%2Cw_300/MTIwNjA4NjMzNzM0NDY1MDM2/antonio-banderas-9197649-1-402.jpg',
+		scores: [
+			'5',
+			'2',
+			'3',
+			'1',
+			'5',
+			'1',
+			'3',
+			'1',
+			'1',
+			'5'
+		]
+	},
+	{
+		name: 'Jaime',
+		photo: 'https://vignette.wikia.nocookie.net/gameofthrones/images/e/eb/Jaime.jpg/revision/latest/scale-to-width-down/296?cb=20170818052054',
+		scores: [
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5'
+		]
+	},
+	{
+		name: 'Merlin',
+		photo: 'https://d1nao0k9edgivc.cloudfront.net/wp-content/uploads/2015/11/Merlin-movie-300x200.jpg',
+		scores: [
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1'
+		]
+	}
+];
+
+module.exports = friendsArray;
---
+++ app/Routing/apiRoutes.js
@@ -0,0 +1,17 @@
+// Routes - takes in JSON input
+var path = require("path");
+
+var friends = require('../data/friends.js');
+
+module.exports = function(app){
+    app.get("/api/friends", function(req, res) {
+
+        res.json(friends);
+      });
+    app.post("/api/friends", function(req, res) {
+
+        
+
+    res.json(friend);
+  });
+};
